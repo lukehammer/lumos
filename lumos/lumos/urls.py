fbe8f11675d02d9d8801ee3cda87532d036b586b
luke hammer <luke@lukehammermagic.com>
2014-12-22 19:08:22 -0800

    moving to chapter 4 TWD.  hello world basic pages working ok

--- lumos/lumos/urls.py
+++ lumos/lumos/urls.py
@@ -1,12 +1,13 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 from lumos import views
 
- Uncomment the next two lines to enable the admin:
- from django.contrib import admin
- admin.autodiscover()
+ #Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', views.index,name= index''))
+    url(r'^$', views.index, name= 'index'),
+    url(r'^monkeys$', views.monkeys, name= 'monkeys'))
 
     # Examples:
     # url(r'^$', 'lumos.views.home', name='home'),
@@ -17,5 +18,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-)
+
 #nothing changed 
--- lumos/lumos/views.py
+++ lumos/lumos/views.py
@@ -2,3 +2,5 @@ from django.http import HttpResponse
 
 def index(resuest):
     return HttpResponse("lumos says hello world!")
+def monkeys(resuest):
+    return HttpResponse("the monkeys are after you")
