b95635a8355d540be994f7a8a776b93d1586b4fe
luke hammer <luke@lukehammermagic.com>
2015-01-28 18:47:15 -0800

    still working on tango with django

--- lumos/lumos/settings.py
+++ lumos/lumos/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+STATIC_PATH = os.path.join(BASE_DIR,'static')
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
